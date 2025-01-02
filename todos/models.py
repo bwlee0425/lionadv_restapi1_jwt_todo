605ff2d53d506fe1feab69744ee6544cec3f858a
bwlee0425 <bwlee0425@gmail.com>
2025-01-02 06:49:50 +0000

    todos

---
+++ todos/models.py
@@ -0,0 +1,18 @@
+# todos/models.py
+from django.db import models
+from django.conf import settings
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_completed = models.BooleanField(default=False)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='todos'
+    )
+
+    def __str__(self):
+        return self.title
